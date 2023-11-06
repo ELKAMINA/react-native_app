ceb96d8bf3fd305eba22e201254f6a5fe97b199b
EL KHATTABI <elkhattabi.amina@gmail.com>
2023-11-06 10:29:26 +0100

    till job details

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, Image } from "react-native";
 
-import styles from './company.style'
+import styles from "./company.style";
+import { icons } from "../../../constants";
+import { checkImageURL } from "../../../utils";
 
-const Company = () => {
-  return (
-    <View>
-      <Text>Company</Text>
+const Company = ({ companyLogo, jobTitle, companyName, location }) => {
+ return (
+  <View style={styles.container}>
+   <View style={styles.logoBox}>
+    <Image
+     source={{
+      uri: checkImageURL(companyLogo)
+       ? companyLogo
+       : "https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg",
+     }}
+     style={styles.logoImage}
+    />
+   </View>
+
+   <View style={styles.jobTitleBox}>
+    <Text style={styles.jobTitle}>{jobTitle}</Text>
+   </View>
+
+   <View style={styles.companyInfoBox}>
+    <Text style={styles.companyName}>{companyName} / </Text>
+    <View style={styles.locationBox}>
+     <Image
+      source={icons.location}
+      resizeMode="contain"
+      style={styles.locationImage}
+     />
+     <Text style={styles.locationName}>{location}</Text>
     </View>
-  )
-}
+   </View>
+  </View>
+ );
+};
 
-export default Company
+export default Company;
